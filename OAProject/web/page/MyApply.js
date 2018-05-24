9552703d42d01bc1414ce35ef43331f13e3948f7
jianfengZhang <zjf920507@163.com>
2018-05-24 15:16:28 +0800

    no message

---
+++ OAProject/web/page/MyApply.js
@@ -0,0 +1,17 @@
+import BaseComponent from "./base/BaseComponent";
+import React from "react";
+import {View} from "react-native";
+
+
+
+export default class MyApply extends BaseComponent{
+    _render(){
+        return(<View>
+            
+
+
+
+        </View>)
+    }
+
+}
